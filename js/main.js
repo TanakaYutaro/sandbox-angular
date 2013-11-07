e3c3cd7bb79f1f2736828cc171beffa255dcf1d6
yutaro <y.tanaka.kcbbf@gmail.com>
2013-11-07 21:16:26 +0900

    最低限のテンプレートに初期状態を修正

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-/**
- * Created with JetBrains WebStorm.
- * User: tanaka_yut
- * Date: 13/10/24
- * Time: 16:56
- * To change this template use File | Settings | File Templates.
- */
-function Controller($scope) {
+function Ctrl($scope) {
     $scope.text = "Hello world!";
-    $scope.userList = [
-        "tanaka",
-        "baba",
-        "mori"
-    ];
-    $scope.catList = [
-        { kind : "ノルウェーなんとかキャット", spec : "興味ない"},
-        { kind : "ロシアンブル", spec : "好き"}
-    ]
-    $scope.add = function () {
-        $scope.userList.push($scope.text);
-    }
 }

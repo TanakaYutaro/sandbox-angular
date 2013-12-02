cf5e56be6d28916854a860796a9d8d1180c1f968
Vatscy <bascorpde.crisis@gmail.com>
2013-12-02 21:13:45 +0900

    検索フォーム作成

--- js/main.js
+++ js/main.js
@@ -4,19 +4,57 @@ angular.module("testApp", ["ngRoute"], function ($routeProvider, $locationProvid
             templateUrl: "/template/foo.html"
         })
         .when("/foo/:fooId", {
-            templateUrl: "/template/foo-detail.html",
-            controller: "fooController"
+            templateUrl: "/template/foo-detail.html"
         })
         .otherwise({
             templateUrl: "/template/main.html"
         });
     $locationProvider.html5Mode(true);
-});
+})
+    .controller("FooController", FooController)
+    .controller("SearchController", SearchController);
 
-function topController($scope) {
-    $scope.username = "vatscy";
+function FooController($scope, $routeParams) {
+    $scope.fooId = $routeParams.fooId;
 }
 
-function fooController($scope, $routeParams) {
-    $scope.fooId = $routeParams.fooId;
+function SearchController($scope) {
+    $scope.users = [
+        {
+            id: 1,
+            name: "空条承太郎",
+            kana: "クウジョウジョウタロウ",
+            note: "オラオラ"
+        },
+        {
+            id: 3,
+            name: "東方仗助",
+            kana: "ヒガシカタジョウスケ",
+            note: "変な髪型"
+        },
+        {
+            id: 2,
+            name: "荒木飛呂彦",
+            kana: "アラキヒロヒコ",
+            note: "歳を取らない"
+        },
+        {
+            id: 4,
+            name: "虹村億泰",
+            kana: "ニジムラオクヤス",
+            note: "東方仗助が岸辺露伴を困らせた時わたしは焼身自殺します"
+        },
+        {
+            id: 6,
+            name: "岸辺露伴",
+            kana: "キシベロハン",
+            note: "岸部露伴は動かない"
+        },
+        {
+            id: 5,
+            name: "広瀬康一",
+            kana: "ヒロセコウイチ",
+            note: "エコーズ"
+        }
+    ];
 }

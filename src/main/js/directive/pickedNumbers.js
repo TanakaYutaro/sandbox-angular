71d2215ed03a70ad17c35890dd1be9f2966a87f4
yutaro <y.tanaka.kcbbf@gmail.com>
2013-12-23 01:47:56 +0900

    出目番号を整形して表示（番号によって色変えるのは未完）

--- src/main/js/directive/pickedNumbers.js
+++ src/main/js/directive/pickedNumbers.js
@@ -6,23 +6,23 @@ function PickedNumbers() {
             var pickedNumber = parseInt(attrs.num);
 
             if (1 <= pickedNumber && pickedNumber <= 9) {
-                element.addClass('badge');
+                element.addClass('cube-gray');
             } else if (10 <= pickedNumber && pickedNumber <= 19) {
-                element.addClass('badge badge-success');
+                element.addClass('cube-red');
             } else if (20 <= pickedNumber && pickedNumber <= 29) {
-                element.addClass('badge badge-warning');
+                element.addClass('cube-blue');
             } else if (30 <= pickedNumber && pickedNumber <= 39) {
-                element.addClass('badge badge-important');
+                element.addClass('cube-green');
             } else if (40 <= pickedNumber && pickedNumber <= 49) {
-                element.addClass('badge badge-info');
+                element.addClass('cube-yellow');
             } else if (50 <= pickedNumber && pickedNumber <= 59) {
-                element.addClass('badge badge-inverse');
+                element.addClass('cube-black');
             } else if (60 <= pickedNumber && pickedNumber <= 69) {
-                element.addClass('badge');
+                element.addClass('cube');
             } else if (70 <= pickedNumber && pickedNumber <= 75) {
-                element.addClass('badge');
+                element.addClass('cube');
             } else {
-                element.addClass('badge badge-inverse');
+                element.addClass('cube2');
             }
         }
     }

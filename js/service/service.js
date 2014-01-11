3ea6a09cbddc7b7d7a37c9374e0dbd3b9718fd3f
yutaro <y.tanaka.kcbbf@gmail.com>
2014-01-11 23:04:04 +0900

    AND検索用のオブジェクト探索サービスを追加
    再帰処理を用いて、オブジェクト内の全階層に対して
    値がキーワードと部分一致するかどうか判定

--- js/service/service.js
+++ js/service/service.js
@@ -49,3 +49,35 @@ SearchService.prototype.getData = function () {
         }
     ];
 };
+
+/**
+ * オブジェクト内の値がキーワードと部分一致するかどうか判定
+ * @param obj 検索対象オブジェクト
+ * @param keyword キーワード配列
+ * @returns {boolean}
+ */
+SearchService.prototype.keywordJudge = function (obj, keyword) {
+    var self = this;
+
+    if (angular.isArray(obj)) {
+        // 配列の場合
+        // 格納されている要素を順番にチェックし、ひとつでも部分一致した場合trueを返す
+        return obj.some(function (child) {
+            return self.keywordJudge(child, keyword);
+        });
+    } else if (angular.isObject(obj)) {
+        // オブジェクトの場合
+        // 子要素を順番にチェックし、ひとつでも部分一致した場合trueを返す
+        var properties = Object.getOwnPropertyNames(obj);
+        return properties.some(function (property) {
+            var child = obj[property];
+            return self.keywordJudge(child, keyword);
+        });
+    } else if (obj != null) {
+        // オブジェクト、配列以外で、値がある場合
+        // 文字列に変換し、部分一致した場合trueを返す
+        return angular.toJson(obj).search(keyword) != -1;
+    }
+    // nullまたはundefinedの場合
+    return false;
+};

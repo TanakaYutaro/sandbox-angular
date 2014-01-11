a3deab02e7c09d799e8f7e58c4bf0756189e6629
yutaro <y.tanaka.kcbbf@gmail.com>
2014-01-11 23:14:23 +0900

    AND検索フィルタを適応

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 angular.module("SearchApp", [])
     .service("searchService", SearchService)
+    .filter("andSearchFilter", ANDSearchFilter)
     .controller("searchController", SearchController);
 
 

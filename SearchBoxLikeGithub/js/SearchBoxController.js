461daa4bc321b3946576158ce4cf2d35da3f99d6
yutaro <yutaro.tanaka.world@gmail.com>
2015-05-17 16:10:21 +0900

    Add filter for refine search
    click filter list, you can change filter target.

--- SearchBoxLikeGithub/js/SearchBoxController.js
+++ SearchBoxLikeGithub/js/SearchBoxController.js
@@ -1,3 +1,29 @@
-function SearchBoxController(searchBoxService) {
-    this.users = searchBoxService.getData();
+function SearchBoxController($timeout, searchBoxService) {
+    var self = this;
+
+    self.$timeout = $timeout;
+    self.users = searchBoxService.getData();
+
+    self.keyword = {
+        id: '',
+        name: '',
+        kana: '',
+        note: '',
+        $: ''
+    };
+
+    self.filterLabel = {
+        id: 'ID',
+        name: '氏名',
+        kana: 'フリガナ',
+        note: '備考',
+        $: 'ALL'
+    };
 }
+
+SearchBoxController.prototype.switchFilter = function (target) {
+    this.filter = target;
+    for (var k in self.keyword) {
+        this.keyword[k] = '';
+    }
+};

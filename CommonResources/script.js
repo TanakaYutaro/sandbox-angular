6145a994f16eb6f310831f234db273f623fdb1eb
yutaro <yutaro.tanaka.world@gmail.com>
2015-06-11 11:30:38 +0900

    Extract to abstract

--- CommonResources/script.js
+++ CommonResources/script.js
@@ -1,19 +1,61 @@
 angular.module("MyApp", ["ngResource"])
-    .factory('User', function ($resource) {
-        return {
-            'getDetails': function (successCallback, errorCallback) {
-                $resource('http://www.mocky.io/v2/557824cd316006411332c4f1').get()
-                    .$promise.then(successCallback, errorCallback);
-            }
+    .factory('AbstractModel', function () {
+        var Model = function (defaults, resource) {
+            this.data = defaults;
+            this.resource = resource;
+        };
+
+        Model.prototype.fetch = function () {
+            var self = this;
+
+            this.resource.query(function (result) {
+                self.data = result.data;
+            });
+        };
+
+        return Model;
+    })
+    .factory('AbstractResource', function ($resource) {
+        var baseURL = 'http://www.mocky.io',
+            baseParams = {
+                callback: 'JSON_CALLBACK'
+            },
+            baseOptions = {
+                query: {
+                    method: 'JSONP'
+                }
+            };
+
+        return function (endpoint, params, options) {
+            var url = baseURL + endpoint,
+                p = angular.extend(params, baseParams),
+                o = angular.extend(options, baseOptions);
+
+            return $resource(url, p, o);
         };
     })
-    .controller("MainController", function (User) {
-        var self = this;
+    .factory('UserResource', function (AbstractResource) {
+        var endpoint = "/v2/5578f13ebdbf289403423c57";
+        var params = {};
+        var options = {};
 
-        User.getDetails(function (result) {
-            self.hoge = result.name;
-            console.log("success");
-        }, function () {
-            console.log("error");
-        });
+        return new AbstractResource(endpoint, params, options);
+    })
+    .factory('UserModel', function (AbstractModel, UserResource) {
+
+        var defaults = {
+            name: "GeoGeo"
+        };
+
+        return new AbstractModel(defaults, UserResource);
+    })
+    .controller("MainController", function ($scope, UserModel) {
+        this.user = UserModel;
+
+        this.reload = function () {
+            UserModel.fetch();
+        }
+    })
+    .controller("SubController", function ($scope, UserModel) {
+        this.user = UserModel;
     });

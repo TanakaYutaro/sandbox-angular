d43024ebe6654c24028ba061d52433b43cd1524c
yutaro <yutaro.tanaka.world@gmail.com>
2015-06-14 18:15:25 +0900

    Create new todo

--- TODO/script.js
+++ TODO/script.js
@@ -1,16 +1,11 @@
 angular.module('TODO', [])
     .controller('TodoController', function () {
-        this.todos = [
-            'システム企画',
-            '要件定義',
-            '概要設計',
-            '詳細設計',
-            'プログラム設計',
-            'プログラミング',
-            'プログラムテスト',
-            '結合テスト',
-            '受入テスト',
-            '運用テスト',
-            'リリース'
-        ];
+        var self = this;
+
+        self.todos = [];
+
+        self.create = function() {
+            self.todos.push(self.newTodo);
+            self.newTodo = '';
+        }
     });

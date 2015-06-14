da0bce25b8382d3550a23accab9e935028dd67f5
yutaro <yutaro.tanaka.world@gmail.com>
2015-06-14 19:31:59 +0900

    Archive

--- TODO/script.js
+++ TODO/script.js
@@ -7,12 +7,18 @@ angular.module('TODO', [])
             {title: '要件定義', done: false}
         ];
 
-        self.create = function() {
-            var todo = {
-                title: self.newTodo,
-                done: false
-            };
-            self.todos.push(todo);
+        self.create = function () {
+            self.todos.push({title: self.newTodo, done: false});
             self.newTodo = '';
+        };
+
+        self.archive = function () {
+            var currentTodo = self.todos;
+            self.todos = [];
+            angular.forEach(currentTodo, function (todo) {
+                if(!todo.done) {
+                    self.todos.push(todo);
+                }
+            });
         };
     });

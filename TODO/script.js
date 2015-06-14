aef6e017b44993c82ba4ac073cb85b81f9e9cd30
yutaro <yutaro.tanaka.world@gmail.com>
2015-06-14 19:07:28 +0900

    todo done checking

--- TODO/script.js
+++ TODO/script.js
@@ -2,10 +2,17 @@ angular.module('TODO', [])
     .controller('TodoController', function () {
         var self = this;
 
-        self.todos = [];
+        self.todos = [
+            {title: 'システム企画', done: true},
+            {title: '要件定義', done: false}
+        ];
 
         self.create = function() {
-            self.todos.push(self.newTodo);
+            var todo = {
+                title: self.newTodo,
+                done: false
+            };
+            self.todos.push(todo);
             self.newTodo = '';
-        }
+        };
     });

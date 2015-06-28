6fac5b9b9968a357e886741346e679bfd9d6a8af
yutaro <yutaro.tanaka.world@gmail.com>
2015-06-29 01:13:34 +0900

    using directive

--- Form/script.js
+++ Form/script.js
@@ -6,4 +6,38 @@ angular.module("MyApp", [])
                 message: 'メールアドレス形式が不正です。'
             }
         };
+    })
+    .directive('myInput', function () {
+        return {
+            restrict: 'A',
+            require: ['^?form'],
+            transclude: true,
+            replace: true,
+            scope: {
+                title: '@',
+                name: '@',
+                placeholder: '@',
+                ngModel: '=',
+                required: '=',
+                validation: '='
+            },
+            link: function (scope, element, attrs, controllers) {
+                var target = controllers[0][attrs.name];
+
+                scope.isError = function () {
+                    return target.$dirty && target.$invalid;
+                };
+
+                scope.isRequiredError = function () {
+                    return target.$error.required;
+                };
+
+                scope.isInvalidError = function () {
+                    return !scope.isRequiredError()
+                        && target.$invalid
+                        && scope.validation.message;
+                };
+            },
+            templateUrl: "template/template.html"
+        };
     });
